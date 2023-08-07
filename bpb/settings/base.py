d61db516b18920664e3fbd4f071285d70547b51e
Anton Balabanov <asbbnv@gmail.com>
2023-08-07 22:38:19 +0300

    restrictive default

--- bpb/settings/base.py
+++ bpb/settings/base.py
@@ -15,15 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=17bwqk5c3sn&l7!jguy@n1j_jcoc&q1ikvmjgb(f4-$wimii*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
